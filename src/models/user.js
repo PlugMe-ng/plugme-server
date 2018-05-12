356370703113b1c9a8511daea1b9fa09a5cebc26
Franklin Chieze <chieze.franklin@gmail.com>
2018-05-12 18:24:54 +0100

    chore(sequelize): set up sequelize
    - set up sequelize (ensure Postgres is installed locally and on Heroku)
    - create a basic User model
    
    [Finishes #157529568]

---
+++ src/models/user.js
@@ -0,0 +1,54 @@
+/**
+ * @fileOverview user model
+ *
+ * @author Franklin Chieze
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    googleId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    role: {
+      type: DataTypes.ENUM,
+      values: [
+        'admin', 'disabled', 'member'
+      ],
+      defaultValue: 'member'
+    },
+  });
+
+  User.associate = (models) => {};
+
+  return User;
+};
