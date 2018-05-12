a3b2e8b9267a8f98e36a0cb8c231902caf8a3551
Franklin Chieze <chieze.franklin@gmail.com>
2018-05-12 07:01:04 +0100

    setup CORS using npm package 'cors'

--- src/server.js
+++ src/server.js
@@ -1,10 +1,38 @@
+/**
+ * @fileOverview Application's entry point
+ *
+ * @author Franklin Chieze
+ *
+ * @requires NPM:body-parser
+ * @requires NPM:cors
+ * @requires NPM:dotenv
+ * @requires NPM:express
+ * @requires NPM:morgan
+ * @requires NPM:path
+ * @requires NPM:swagger-jsdoc
+ * @requires ./middleware
+ * @requires ./routes
+ */
+
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
 
 const app = express();
+
+// CORS
+/*
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin');
+  res.header('Access-Control-Allow-Methods', 'DELETE, GET, POST, PUT');
+  next();
+});
+*/
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
