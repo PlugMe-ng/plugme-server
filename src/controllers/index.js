6455243636acd265dd8e36ebe054a7e76dc25bed
Franklin Chieze <chieze.franklin@gmail.com>
2018-05-29 12:29:06 +0100

    add code to /controllers/Users.js

---
+++ src/controllers/index.js
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview index file for controllers
+ *
+ * @author Franklin Chieze
+ *
+ * @requires ./Auth
+ * @requires ./Users
+ */
+
+import Auth from './Auth';
+import Users from './Users';
+
+export default {
+  Auth,
+  Users
+};
