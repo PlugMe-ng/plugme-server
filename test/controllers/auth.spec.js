f74d2359fc31419c8d73bf8adeb3688de3123ecd
Franklin Chieze <chieze.franklin@gmail.com>
2018-06-01 20:41:53 +0100

    add test file for Auth controller

---
+++ test/controllers/auth.spec.js
@@ -0,0 +1,303 @@
+/**
+ * @fileOverview Auth controller tests
+ *
+ * @author Franklin Chieze
+ *
+ * @requires NPM:chai
+ * @requires NPM:chai-http
+ * @requires ../mock
+ * @requires ../../build/models
+ * @requires ../../build/server
+ */
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import mock from '../mock';
+import models from '../../build/models';
+import server from '../../build/server';
+
+const should = chai.should();
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('AuthController', () => {
+  beforeEach(async () => {
+    await models.User.destroy({ where: {} });
+  });
+
+  describe('POST: /v1/auth/signin', (done) => {
+    beforeEach(async () => {
+      await models.User.create(mock.user1);
+    });
+    it('should return a user token on successful sign in', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signin')
+        .send(mock.user1)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          expect(res.body.data).to.be.an('Object');
+          res.body.data.should.have.property('user');
+          expect(res.body.data.user.id).to.not.be.undefined;
+          expect(res.body.data.user.displayName)
+            .to.equal(mock.user1.displayName);
+          expect(res.body.data.user.email).to.equal(mock.user1.email);
+          res.body.data.should.have.property('userToken');
+          expect(res.body.errors).to.be.undefined;
+          done();
+        });
+    });
+    it('should not sign in user without display name', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signin')
+        .send(mock.user1WithoutDisplayName)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The displayName field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not sign in user without email', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signin')
+        .send(mock.user1WithoutEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not sign in user with malformed email', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signin')
+        .send(mock.user1WithMalformedEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email format is invalid.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not sign in user with malformed email [2]', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signin')
+        .send(mock.user2WithMalformedEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email format is invalid.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not sign in user with malformed email [3]', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signin')
+        .send(mock.user3WithMalformedEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email format is invalid.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not sign in user without google id', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signin')
+        .send(mock.user1WithoutGoogleId)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The googleId field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+  });
+
+  describe('POST: /v1/auth/signup', (done) => {
+    it('should register a new user', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('data');
+          expect(res.body.data).to.be.an('Object');
+          res.body.data.should.have.property('user');
+          expect(res.body.data.user.id).to.not.be.undefined;
+          expect(res.body.data.user.displayName)
+            .to.equal(mock.user1.displayName);
+          expect(res.body.data.user.email).to.equal(mock.user1.email);
+          res.body.data.should.have.property('userToken');
+          expect(res.body.errors).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user without display name', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1WithoutDisplayName)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The displayName field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user without email', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1WithoutEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user with malformed email', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1WithMalformedEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email format is invalid.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user with malformed email [2]', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user2WithMalformedEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email format is invalid.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user with malformed email [3]', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user3WithMalformedEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The email format is invalid.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user with non-andela email', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1WithNonAndelaEmail)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('Email address must be @andela.com.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user without github username', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1WithoutGithubUsername)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The githubUsername field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user without google id', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1WithoutGoogleId)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The googleId field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+    it('should not register a user without slack id', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1WithoutSlackId)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('The slackId field is required.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+  });
+
+  describe('POST: /v1/auth/signup', (done) => {
+    beforeEach(async () => {
+      await models.User.create(mock.user1);
+    });
+    it('should not register a user with an existing email', (done) => {
+      chai.request(server)
+        .post('/v1/auth/signup')
+        .send(mock.user1)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('errors');
+          expect(res.body.errors).to.be.an('Array');
+          expect(res.body.errors)
+            .to.include('User with the same email already exists.');
+          expect(res.body.data).to.be.undefined;
+          done();
+        });
+    });
+  });
+});
---
+++ test/mock/index.js
@@ -0,0 +1,155 @@
+export default {
+  team1: {
+    name: 'team1',
+    description: 'description of team1',
+    photo: 'https://www.myphotos.com/1',
+  },
+  team2: {
+    name: 'team2',
+    description: 'description of team2',
+    photo: 'https://www.myphotos.com/2',
+    private: true,
+    progress: 50
+  },
+  team3: {
+    name: 'team3',
+    description: 'description of team3',
+    photo: 'https://www.myphotos.com/3',
+  },
+  team4: {
+    name: 'team4',
+    description: 'description of team4',
+    photo: 'https://www.myphotos.com/4',
+  },
+  team5: {
+    name: 'team5',
+    description: 'description of team5',
+    photo: 'https://www.myphotos.com/5',
+  },
+  team1WithoutOptionalProperties: {
+    name: 'team1'
+  },
+  team2WithoutOptionalProperties: {
+    name: 'team2'
+  },
+  team1WithoutName: {
+    description: 'description of team1',
+    photo: 'https://www.myphotos.com/1',
+  },
+  user0: {
+    displayName: 'user0',
+    email: 'user0@andela.com',
+    githubUsername: 'user0',
+    googleId: 123450,
+    photo: 'https://www.myphotos.com/0',
+    role: 'admin',
+    slackId: 1234560
+  },
+  user1: {
+    displayName: 'user1',
+    email: 'user1@andela.com',
+    githubUsername: 'user1',
+    googleId: 123451,
+    photo: 'https://www.myphotos.com/1',
+    slackId: 1234561
+  },
+  user2: {
+    displayName: 'user2',
+    email: 'user2@andela.com',
+    githubUsername: 'user2',
+    googleId: 123452,
+    photo: 'https://www.myphotos.com/2',
+    slackId: 1234562
+  },
+  user3: {
+    displayName: 'user3',
+    email: 'user3@andela.com',
+    githubUsername: 'user3',
+    googleId: 123453,
+    photo: 'https://www.myphotos.com/3',
+    slackId: 1234563
+  },
+  user4: {
+    displayName: 'user4',
+    email: 'user4@andela.com',
+    githubUsername: 'user4',
+    googleId: 123454,
+    photo: 'https://www.myphotos.com/4',
+    slackId: 1234564
+  },
+  user5: {
+    displayName: 'user5',
+    email: 'user5@andela.com',
+    githubUsername: 'user5',
+    googleId: 123455,
+    photo: 'https://www.myphotos.com/5',
+    slackId: 1234565
+  },
+  user1WithNonAndelaEmail: {
+    displayName: 'user1',
+    email: 'user1@email.com',
+    githubUsername: 'user1',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/1',
+    slackId: 123456
+  },
+  user1WithMalformedEmail: {
+    displayName: 'user1',
+    email: 'user1@andelacom',
+    githubUsername: 'user1',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/1',
+    slackId: 123456
+  },
+  user2WithMalformedEmail: {
+    displayName: 'user2',
+    email: 'user2andela.com',
+    githubUsername: 'user2',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/2',
+    slackId: 123456
+  },
+  user3WithMalformedEmail: {
+    displayName: 'user3',
+    email: 'user3@andela.',
+    githubUsername: 'user3',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/3',
+    slackId: 123456
+  },
+  user1WithoutEmail: {
+    displayName: 'user1',
+    githubUsername: 'user1',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/1',
+    slackId: 123456
+  },
+  user1WithoutDisplayName: {
+    email: 'user1@andela.com',
+    githubUsername: 'user1',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/1',
+    slackId: 123456
+  },
+  user1WithoutGithubUsername: {
+    displayName: 'user1',
+    email: 'user1@andela.com',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/1',
+    slackId: 123456
+  },
+  user1WithoutGoogleId: {
+    displayName: 'user1',
+    email: 'user1@andela.com',
+    githubUsername: 'user1',
+    photo: 'https://www.myphotos.com/1',
+    slackId: 123456
+  },
+  user1WithoutSlackId: {
+    displayName: 'user1',
+    email: 'user1@andela.com',
+    githubUsername: 'user1',
+    googleId: 12345,
+    photo: 'https://www.myphotos.com/1',
+  },
+};
