0f66ece284ce04df034649421ba50364d35a078b
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2018-07-20 23:12:49 +0100

    add body trimmer for requests

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ import express from 'express';
 import logger from 'morgan';
 import path from 'path';
 import swaggerUI from 'swagger-ui-express';
+import trimmer from 'express-trimmer';
 
 import middleware from './middleware';
 import routes from './routes';
@@ -57,17 +58,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apiDocs));
 
+app.use(middleware.api);
+
 // set content type
 app.use((req, res, next) => {
   res.header('Content-Type', 'application/json');
   next();
 });
 
-app.use(middleware.api);
-
 app.get('/', (req, res) => {
   res.sendSuccess(['Welcome to PlugMe Server']);
 });
+
+app.use(trimmer);
 
 routes(app);
 
